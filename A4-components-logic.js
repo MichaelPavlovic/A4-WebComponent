9417ca6dd1b8c0eb75d05fdf5af0fe2c9b341ce9
Michael Pavlovic <michaelpavlovic87@gmail.com>
2020-03-22 20:59:31 -0400

    Add project files

---
+++ A4-components-logic.js
@@ -0,0 +1,44 @@
+﻿//ASSIGNMENT 4 WEB COMPONENTS
+//LOGIC FILE
+//IN THIS FILE YOU WILL SIMPLY GRAB YOUR CUSTOM ELEMENT AND ATTACH YOUR METHOD FROM YOUR MODULE TO IT.
+class HumberClock extends HTMLElement{
+    constructor(){
+        super();
+
+        let template = document.getElementById('humber-clock');
+        let templateContent = template.content;
+
+        const shadowRoot = this.attachShadow({ mode:'open' });
+
+        //get external style sheet
+        const style = document.createElement('link');
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', 'A4-components.css');
+
+        //appent to shadow root
+        shadowRoot.appendChild(style);
+        shadowRoot.appendChild(templateContent.cloneNode(true));
+    }
+    connectedCallback(){
+        const count = this.shadowRoot.getElementById('countdown');
+
+        setInterval(function(){
+            let current = new Date();
+            let hours = current.getHours();
+            let minutes = current.getMinutes();
+            let seconds = current.getSeconds();
+
+            if(minutes < 10 ){
+                minutes = '0' + minutes;
+            }
+
+            if(seconds < 10){
+                seconds = '0' + seconds;
+            }
+
+            count.innerHTML = hours + ":" + minutes + ":" + seconds;
+        }, 1000);
+    }
+}
+
+customElements.define('humber-clock', HumberClock);
---
+++ A4-components-module.js
@@ -0,0 +1,18 @@
+﻿//ASSIGNMENT 4 WEB COMPONENTS
+//MODULE FILE
+//IN THIS FILE YOU WILL CREATE YOUR CUSTOM MODULE FOR YOUR REQUIRED FUNCTIONALITY AND EXPOSE IT THROUGH A 'PUBLICLY' ACCESSIBLE METHOD.
+var myModule = (function(){
+    function clock(){
+
+    }
+
+    function getTime(){
+        clock();
+    }
+    
+    var module = {
+        getTime: getTime
+    };
+
+    return module;
+})();
